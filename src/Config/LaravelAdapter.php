f8544cc58e85b925fca997c1f0dd62136f1bdaad
VojtaB <brozek@thepay.cz>
2020-12-08 20:16:13 +0100

    add config adapter

---
+++ src/Config/LaravelAdapter.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleAsFuck\LaravelOrm\Config;
+
+use Illuminate\Contracts\Config\Repository;
+use SimpleAsFuck\Orm\Config\Abstracts\Config;
+
+final class LaravelAdapter extends Config
+{
+    private Repository $repository;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getValue(string $key)
+    {
+        $key = 'orm.'.$key;
+        if (! $this->repository->has($key)) {
+            throw new \RuntimeException('Config key: "'.$key.'" not exists');
+        }
+
+        return $this->repository->get($key);
+    }
+}
